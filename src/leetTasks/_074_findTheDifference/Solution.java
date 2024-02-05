8a3b65ace657474c75a7652f8158a733c85393b2
klim-off <investklimov@gmail.com>
2024-02-05 10:34:55 +0300

    _34 commit

---
+++ src/leetTasks/_074_findTheDifference/Solution.java
@@ -0,0 +1,39 @@
+package leetTasks._074_findTheDifference;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String s = "abcd";
+        String t = "abcde";
+        System.out.println(findTheDifference(s,t));
+    }
+
+    public static char findTheDifference(String s, String t) {
+
+        char c = (char) 0;
+        for(int i=0;i<s.length();i++) {
+            c ^= s.charAt(i);
+            System.out.println(c);
+        }
+
+        for(int i=0;i<t.length();i++) {
+            c ^= t.charAt(i);
+            System.out.println(c);
+        }
+        return c;
+
+       /* StringBuilder builderT = new StringBuilder(t);
+         for (int i = 0; i < s.length(); i++) {
+         if( builderT.indexOf(String.valueOf(s.charAt(i))) == -1){
+             return t.charAt(i);
+         }else{
+             builderT.deleteCharAt(builderT.indexOf(String.valueOf(s.charAt(i))));
+
+         }
+        }
+        return builderT.charAt(0);*/
+    }
+}
