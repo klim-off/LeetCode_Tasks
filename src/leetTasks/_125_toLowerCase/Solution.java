f710a37fd2afbae3807aeb82f28bb1232d5f9e01
klim-off <investklimov@gmail.com>
2024-04-21 06:28:27 +0300

    _94 commit

---
+++ src/leetTasks/_125_toLowerCase/Solution.java
@@ -0,0 +1,15 @@
+package leetTasks._125_toLowerCase;
+
+/*
+Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+ */
+public class Solution {
+    public static void main(String[] args) {
+        String s = "Hello";
+        System.out.println(toLowerCase(s));
+    }
+
+    public static String toLowerCase(String s) {
+     return s.toLowerCase();
+    }
+}
